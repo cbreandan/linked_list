abdc02c538bafbefc3fbfd50ba89a36d1d7ba2e8
Breandan Choi <cbreandan@gmail.com>
2017-05-26 10:53:20 -0400

    overload output

--- linked-list.cpp
+++ linked-list.cpp
@@ -17,8 +17,8 @@ public:
   ~LinkedList();
 
   //operations
-  int size();
-  bool isEmpty();
+  int size() const;
+  bool isEmpty() const;
 
   //insert
   void insertFront(int val);
@@ -27,16 +27,18 @@ public:
 
   //remove - list must be non-empty
   void removeAt(int key);
-  void removeAfter(int key);
+
+  //overload output operator for print
+  friend std::ostream &operator<<(std::ostream &out, const LinkedList LL);
 
   //print
-  void print();
+  void skipPrint() const;
 
   //advanced operations
-  bool isCycle();
+  bool isCycle() const;
 };
 
-int LinkedList::size(){
+int LinkedList::size() const{
   int total = 0;
   if (!head){
 	return 0;
@@ -50,7 +52,7 @@ int LinkedList::size(){
   }
 }
 
-bool LinkedList::isEmpty(){
+bool LinkedList::isEmpty() const{
   size() ? true : false;
 }
 
@@ -99,25 +101,57 @@ void LinkedList::insertAfter(int key, int val){
 }
 
 void LinkedList::removeAt(int key){
-}
-
-void LinkedList::removeAfter(int key){
-
-}
-
-void LinkedList::print(){
-  if (isEmpty){
-	cout << "The list is empty!" << endl;
+  Node *n = head;
+  if (size() == 1){
+	if (n->key == key){
+	  head = nullptr;
+	  tail = nullptr;
+	}
+	else {
+	  cout << "No key found" << endl;
+	}
   }
   else {
-	Node n = head;
 	while (n){
-	  cout << "key: " << n->key << ", " << "value: " << n->val << endl;
+	  if (n->next->key == key){
+		n->next = n->next->next;
+		return;
+	  }
+	  else {
+		n = n->next;
+	  }
+	}
+	cout << "No key found" << endl;
+  }
+}
+
+std::ostream &operator<<(std::ostream &out, const LinkedList LL){
+  if (isEmpty){
+	out << "The list is empty!" << endl;
+  }
+  else {
+	Node *n = head;
+	while (n){
+	  out << "key: " << n->key << ", " << "value: " << n->val << endl;
 	  n = n->next;
 	}
+  }
+  return out;
+}
+
+void LinkedList::skipPrint() const {
+  int count = 2;
+  Node *n = head;
+  while (n){
+	if (count % 2 == 0){
+	  cout << "key: " << n->key << ", value: " << n->val << endl;
+	}
+	count += 1;
+	n = n->next;
   }
 }
 
 int main(){
   LinkedList LL = LinkedList();
+  cout << LL;
 }
